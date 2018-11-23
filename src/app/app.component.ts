70a6294ddcf9e8c51094aeb1ff35e5418f163ae9
gwfernandez <gwfernandez@gmail.com>
2018-11-22 23:12:47 -0300

    configuro bootstrap y google-fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'youtubeapp';
+  title = "youtubeapp";
+  //AIzaSyBvfarg4e24YV7Y3V3ese9S9HzbbjNHtOI
 }
